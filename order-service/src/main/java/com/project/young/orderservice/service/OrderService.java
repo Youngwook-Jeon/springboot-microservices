a1df647c680dfc29b6824673b9dfeb8ffae3057e
Youngwook-Jeon <ela87@naver.com>
2022-09-27 21:44:42 +0900

    Implement circuit breaker pattern by using Resilience4j

--- order-service/src/main/java/com/project/young/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/project/young/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -52,6 +52,7 @@ public class OrderService {
 
         if (allProductsIsInStock) {
             orderRepository.save(order);
+            return "Order Placed successfully";
         } else {
             throw new IllegalArgumentException("At least one product is not in stock, please try again later");
         }
